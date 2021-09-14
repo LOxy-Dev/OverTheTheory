654df9bc3d8846e833791c06908d5a204208dfb3
Hector Fiorese <loxygaming78@gmail.com>
2021-09-14 19:12:58 +0200

    Add heroes icons + very basic hovering feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,113 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
+    <div id="TeamComposition">
+        <h2>Team Composition</h2>
 
+        <div id="Comps">
+            <div className="CompTeam blue">
+                <div className="Tank">
+                    <h3 className="Tank-Title">Tank</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/dva-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/orisa-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/reinhardt-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/roadhog-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/sigma-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/winston-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/wreckingball-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/zarya-icon.png"/>
+                </div>
+
+                <div className="Damage">
+                    <h3 className="Damage-title">DPS</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/ashe-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/bastion-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/doomfist-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/echo-icon.webp"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/genji-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/hanzo-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/junkrat-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mccree-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mei-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/pharah-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/reaper-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/soldier76-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/sombra-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/symmetra-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/torbjorn-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/tracer-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/widowmaker-icon.png"/>
+                </div>
+
+                <div className="Support">
+                    <h3 className={"Support-title"}>Support</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/ana-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/baptiste-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/brigitte-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/lucio-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mercy-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/moira-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/zenyatta-icon.png"/>
+                </div>
+            </div>
+            <div className="CompTeam red">
+                <div className="Tank">
+                    <h3 className="Tank-Title">Tank</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/dva-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/orisa-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/reinhardt-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/roadhog-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/sigma-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/winston-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/wreckingball-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/zarya-icon.png"/>
+                </div>
+
+                <div className="Damage">
+                    <h3 className="Damage-title">DPS</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/ashe-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/bastion-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/doomfist-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/echo-icon.webp"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/genji-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/hanzo-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/junkrat-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mccree-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mei-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/pharah-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/reaper-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/soldier76-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/sombra-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/symmetra-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/torbjorn-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/tracer-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/widowmaker-icon.png"/>
+                </div>
+
+                <div className="Support">
+                    <h3 className={"Support-title"}>Support</h3>
+
+                    <img className="Champion" src="OverTheTheory/Hero-icons/ana-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/baptiste-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/brigitte-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/lucio-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/mercy-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/moira-icon.png"/>
+                    <img className="Champion" src="OverTheTheory/Hero-icons/zenyatta-icon.png"/>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
 
